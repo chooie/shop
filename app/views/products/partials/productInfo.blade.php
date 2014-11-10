9cbc6ab8b4e966ea4af46136d066402f00677a59
Charlie Hebert <chooie@Charlies-MacBook-Pro.local>
2014-11-10 15:25:57 +0000

    Fix minor comment issue on product listing view

--- app/views/products/partials/productInfo.blade.php
+++ app/views/products/partials/productInfo.blade.php
@@ -1,9 +1,9 @@
-{-- TODO: Make this pretty --}
+{{-- TODO: Make this pretty --}}
 <li>{{ $product->name}}
     <ul>
         <li>{{ $product->price }}</li>
         <li>Posted at: {{ $product->created_at }}</li>
         <li>Updated at: {{ $product->updated_at }}</li>
-        <li>Posted by: {{ $product->user()->first()->username }}</li>
+        <li>Last Activity: {{ $product->user()->first()->username }}</li>
     </ul>
 </li>

e794f45bebaa366099aedffe2e7c24a6ad589563
Charlie Hebert <chooie@Charlies-MacBook-Pro.local>
2014-12-06 13:40:25 +0000

    Remove users view

--- app/views/Users/show.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.default')
-
-@section('content')
-    <p>{{ $user->username }}</p>
-    @foreach($user->products()->get() as $product)
-        @include('products.partials.productOnUserProfile')
-    @endforeach
-@stop

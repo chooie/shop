2ed55eb1416b65515677d7c00fec07c6b66724ee
Charlie Hebert <chooie@Charlies-MacBook-Pro.local>
2014-12-07 21:48:22 +0000

    Add random images to products

--- app/views/products/partials/productInfo.blade.php
+++ app/views/products/partials/productInfo.blade.php
@@ -1,6 +1,6 @@
 {{-- TODO: Make this pretty --}}
 <div class="thumbnail">
-    <img src="http://placehold.it/320x150" alt="">
+    <img src={{ "http://lorempixel.com/350/150/abstract/" . rand(0, 10) }} alt="">
     <div class="caption">
         <h4 class="pull-right">&#8364;{{ $product->price }}</h4>
         <h4>{{ link_to_route('products.show', $product->name, $product->id) }}</h4>

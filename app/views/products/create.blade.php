105cd932ffb0869e0cb8752232bb18cb55a1e113
Charlie Hebert <charlie.hebert92@gmail.com>
2014-12-10 14:44:50 +0000

    Don't know what I'm doing anymore

--- app/views/products/create.blade.php
+++ app/views/products/create.blade.php
@@ -1,34 +1,36 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="row">
-	<div class="col-md-6">
-		<h1>Post your Product!</h1>
+<div class="listings" style="margin-top: 50px">
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Post your Product!</h1>
 
-		@include('layouts.partials.errors')
+            @include('layouts.partials.errors')
 
-		{{ Form::open(['route' => 'products.store', 'files' => true]) }}
-			<div class="form-group">
-				{{ Form::label('name', 'Product name:') }}
-				{{ Form::text('name', null, ['class' => 'form-control']) }}
-			</div>
+            {{ Form::open(['route' => 'products.store', 'files' => true]) }}
+                <div class="form-group">
+                    {{ Form::label('name', 'Product name:') }}
+                    {{ Form::text('name', null, ['class' => 'form-control']) }}
+                </div>
 
-			<div class="form-group">
-				{{ Form::label('price', 'Price') }}
-				{{ Form::text('price', null, ['class' => 'form-control',
-				                              'type' => 'number',
-				                              'min' => '0']) }}
-			</div>
+                <div class="form-group">
+                    {{ Form::label('price', 'Price') }}
+                    {{ Form::text('price', null, ['class' => 'form-control',
+                                                  'type' => 'number',
+                                                  'min' => '0']) }}
+                </div>
 
-            <div class="form-group">
-                {{ Form::label('fileName', 'Upload Product Image:') }}
-                {{ Form::file('fileName') }}
-            </div>
+                <div class="form-group">
+                    {{ Form::label('fileName', 'Upload Product Image:') }}
+                    {{ Form::file('fileName') }}
+                </div>
 
-			<div class="form-group">
-				{{ Form::submit('Post Product', ['class' => 'btn btn-primary']) }}
-			</div>
-		{{ Form::close() }}
-	<div>
+                <div class="form-group">
+                    {{ Form::submit('Post Product', ['class' => 'btn btn-success']) }}
+                </div>
+            {{ Form::close() }}
+        <div>
+    </div>
 </div>
 @stop
--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -1,31 +1,33 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>{{ $user->username }}</h1>
-    <p>Number of Products on Sale: {{ $user->products()->count() }}</p>
+    <div class="listings" style="margin-top: 40px">
+        <h1>{{ $user->username }}</h1>
+        <p>Number of Products on Sale: {{ $user->products()->count() }}</p>
 
-    @foreach ($userProducts->chunk(4) as $productSet)
-        <div class="row row-no-top-margin">
-            @foreach ($productSet as $product)
-                <div class="col-md-3">
-                    @include('products.partials.productOnUserProfile')
-                </div>
-            @endforeach
-        </div>
-    @endforeach
-
-    @include('comments.userComments')
-
-    @if ($signedIn)
-        {{ Form::open(['route' => ['user_comment_path'], 'class' => 'comments__create-form']) }}
-            {{ Form::hidden('user_id', $user->id) }}
-            {{ Form::hidden('commenter_id', $currentUser->id) }}
-
-            <!-- Body Form Input -->
-            <div class="form-group">
-                {{ Form::textarea('body', null,
-                    ['class' => 'form-control', 'rows' => 1, 'placeholder' => 'Leave a comment...']) }}
+        @foreach ($userProducts->chunk(4) as $productSet)
+            <div class="row row-no-top-margin">
+                @foreach ($productSet as $product)
+                    <div class="col-md-3">
+                        @include('products.partials.productOnUserProfile')
+                    </div>
+                @endforeach
             </div>
-        {{ Form::close() }}
-    @endif
+        @endforeach
+
+        @include('comments.userComments')
+
+        @if ($signedIn)
+            {{ Form::open(['route' => ['user_comment_path'], 'class' => 'comments__create-form']) }}
+                {{ Form::hidden('user_id', $user->id) }}
+                {{ Form::hidden('commenter_id', $currentUser->id) }}
+
+                <!-- Body Form Input -->
+                <div class="form-group">
+                    {{ Form::textarea('body', null,
+                        ['class' => 'form-control', 'rows' => 1, 'placeholder' => 'Leave a comment...']) }}
+                </div>
+            {{ Form::close() }}
+        @endif
+    </div>
 @stop
